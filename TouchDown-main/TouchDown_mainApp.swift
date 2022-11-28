719b6d4fe4595693494488413deb7d1c5fb521b2
yeomeme <yeomeme@gmail.com>
2022-11-28 17:05:58 +0900

    Initial Commit

---
+++ TouchDown-main/TouchDown_mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDown_mainApp.swift
+//  TouchDown-main
+//
+//  Created by yeomim Kim on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct TouchDown_mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
