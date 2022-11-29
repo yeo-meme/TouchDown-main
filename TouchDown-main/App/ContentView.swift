e279137d1532dd42f470311fc80d9ca9e7e012a4
yeomeme <yeomeme@gmail.com>
2022-11-29 16:40:27 +0900

    네비게이션바 노치 가리는거 대응

--- TouchDown-main/App/ContentView.swift
+++ TouchDown-main/App/ContentView.swift
@@ -14,8 +14,30 @@ struct ContentView: View {
         // MARK: - PROPERTIES
         
         // MARK: - BODY
-        FooterView()
-        .padding()
+        ZStack {
+            VStack(spacing: 0) {
+                
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                //네이게이션 노치 적용
+                    .padding(.top,
+                             UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05),
+                            radius: 5,x: 0, y:5)
+                
+                //하단에 배치된 푸터뷰로 백그라운영역을 잡고
+                //스페이서로 밀면서 상단여백이 생긴다
+                Spacer()
+                
+                
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackgournd.ignoresSafeArea(.all, edges: .all))
+        } // : ZSTACK
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
