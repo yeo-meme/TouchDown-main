f8e3afb2fd60e1fcefab7440885ab0c713319a0d
yeomeme <yeomeme@gmail.com>
2022-11-29 16:02:07 +0900

    네비게이션뷰 애니메이션적용

--- TouchDown-main/View/Home/NavigationBarView.swift
+++ TouchDown-main/View/Home/NavigationBarView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct NavigationBarView: View {
     // MARK: - PROPERTIES
     
+    
+    @State private var isAnimated : Bool = false
+    
     // MARK: - BODY
     var body: some View {
         HStack{
@@ -23,6 +26,13 @@ struct NavigationBarView: View {
             Spacer()
             
             LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
             
             Spacer()
             
